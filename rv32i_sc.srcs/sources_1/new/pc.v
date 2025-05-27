6ecd45d8318b83ea435b0b5a9da792377b3aa8e0
szymek1 <bogusszymon2000@gmail.com>
2025-05-27 17:38:06 +0200

    Added PC implementation

---
+++ rv32i_sc.srcs/sources_1/new/pc.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 05/27/2025 05:13:17 PM
+// Design Name: 
+// Module Name: pc
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: 32-bit program counter for RV32I
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc(
+    input clk,
+    input rst,
+    input  wire        pc_select, // 1-> PC=PC+4 | 0-> PC=pc_in (accepting new target)
+    input  wire [31:0] pc_in, // branch/jummp target
+    output wire [31:0] pc_out,
+    output wire [31:0] pc_next;
+    );
+    
+    reg [31:0] pc_internal; // current internal value of PC
+    
+    parameter BOOT_ADDR = 32'b0;
+    always @(posedge clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            pc_internal <= BOOT_ADDR;
+        end
+        
+        else begin
+            pc_internal <= pc_select ? pc_in : pc_internal + 32'd4;
+        end
+        
+    end
+
+    assign pc_out  = pc_internal;
+    assign pc_next = pc_out + 32'd4;
+    
+endmodule
