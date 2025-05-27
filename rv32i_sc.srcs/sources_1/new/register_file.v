5f171e90e61ead2ee98bcaef408712e4030e5812
szymek1 <bogusszymon2000@gmail.com>
2025-05-27 14:44:22 +0200

    Implemented first version of register file

--- rv32i_sc.srcs/sources_1/new/register_file.v
+++ rv32i_sc.srcs/sources_1/new/register_file.v
@@ -30,19 +30,37 @@ module register_file(
     input read_enable,
     input  wire  [4:0]  rs1_addr,
     input  wire  [4:0]  rs2_addr,
-    output wire  [31:0] rs1,
-    output wire  [31:0] rs2,
+    output wire  [31:0] rs1, // carries value that has been read with rs1_addr
+    output wire  [31:0] rs2, // carries value that has been read with rs1_addr
     
     // WRITE
     input  wire        write_enable,
-    input  wire [4:0]  write_addr,
-    output wire [31:0] write_data
+    input  wire [4:0]  write_addr, // specifies which register to write to
+    output wire [31:0] write_data  // value to wrtie
     
     );
     
     reg [31:0] registers [31:1]; // skipping x0, which is hard-wired to 32'b0
     
-    assign rs1 = registers[rs1_addr];
-    assign rs2 = registers[rs2_addr];
+    // READ: no further controll is needed here
+    assign rs1 = (rs1_addr != 32'b0) ? registers[rs1_addr]: 32'b0;
+    assign rs2 = (rs2_addr != 32'b0) ? registers[rs2_addr]: 32'b0;
+    
+    // WRITE: clk and rst are the control signals
+    integer reg_id;
+    always @(posedge clk or posedge rst) begin
+    
+        // Reset on rst set
+        if (rst == 1'b0) begin
+            for (reg_id = 0 ; reg_id < 32; reg_id = reg_id + 1) begin
+            registers[reg_id] <= 32'b0;
+            end
+        end
+        
+        else if (write_enable == 1'b1 && write_addr != 32'b0) begin
+            registers[write_addr] <= write_data;
+        end
+        
+    end
     
 endmodule
