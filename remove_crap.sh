4d360015a39022e294e4552fd5bb3ce0b49b2f3f
szymek1 <bogusszymon2000@gmail.com>
2025-05-28 11:52:26 +0200

    Upgrade to removal script

--- remove_crap.sh
+++ remove_crap.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
-rm -r rv32i_sc.cache/
-rm -r rv32i_sc.hw/
-rm -r rv32i_sc.ip_user_files/
-rm -r rv32i_sc.sim/
+
+# List of directories to remove
+dirs=(
+  "rv32i_sc.cache/"
+  "rv32i_sc.hw/"
+  "rv32i_sc.ip_user_files/"
+  "rv32i_sc.sim/"
+)
+
+for dir in "${dirs[@]}"; do
+  if [ -d "$dir" ]; then
+    rm -r "$dir"
+    echo "Removed: $dir"
+  else
+    echo "Skipped (not found): $dir"
+  fi
+done
