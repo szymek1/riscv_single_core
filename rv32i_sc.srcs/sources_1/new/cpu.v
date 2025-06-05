438cd93ca783dbe351573db3fea59095046388ea
szymek1 <bogusszymon2000@gmail.com>
2025-06-05 21:42:19 +0200

    Created main cpu module to begin integration of pc and fetch-stage bram

---
+++ rv32i_sc.srcs/sources_1/new/cpu.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 06/05/2025 09:40:10 PM
+// Design Name: 
+// Module Name: cpu
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: 32-bit, in-order, integer only, RISC-V with 5 stgae pipeline
+// 
+// Dependencies: rv32i_params.vh
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "rv32i_params.vh"
+
+
+module cpu(
+    input clk,
+    input rst
+    );
+endmodule
