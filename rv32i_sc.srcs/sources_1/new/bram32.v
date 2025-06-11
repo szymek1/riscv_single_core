ae80de7b28f5a8b045c3c5e84bd6af8abda14260
szymek1 <bogusszymon2000@gmail.com>
2025-06-11 14:57:11 +0200

    Simple BRAM that outputs in one clock cycle

--- rv32i_sc.srcs/sources_1/new/bram32.v
+++ rv32i_sc.srcs/sources_1/new/bram32.v
@@ -9,7 +9,8 @@
 // Project Name: rv32i_sc
 // Target Devices: Zybo Z7-20
 // Tool Versions: 
-// Description: Custom 32-bit BRAM
+// Description: Custom 32-bit BRAM: currently under the assumption that it returns
+//              within one clock cycle, which is unrealistic.
 // 
 // Dependencies: rv32i_params.vh
 // 
@@ -22,9 +23,36 @@
 
 
 module bram32 (
-    
+    input                         clk,
+    input                         rst,
+    // Write port inputs
+    input  wire [9:0]             w_addr,
+    input  wire [`DATA_WIDTH-1:0] w_dat,
+    input  wire                   w_enb,
+    // Read port inputs
+    input  wire [9:0]             r_addr,
+    // Outputs
+    output reg  [`DATA_WIDTH-1:0] r_dat,
 );
 
+    reg [`DATA_WIDTH-1:0] mem [0:`I_BRAM_DEPTH-1];
     
+    integer i;
+    always @(posedge clk) begin
+        if (rst) begin
+            for (i = 0; i < `I_BRAM_DEPTH; i = i + 1) begin
+                mem[i] <= 32'h0;
+            end
+        end
+        if (!rst && w_enb) begin
+            mem[w_addr] <= w_dat;
+        end
+    end
+    
+    always @(r_addr) begin
+        if (r_enb && !w_enb && !rst) begin
+            r_dat <= mem[r_addr];
+        end
+    end
 
 endmodule
