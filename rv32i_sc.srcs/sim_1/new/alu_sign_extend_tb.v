24f76f03a333e13d77d0cc490b48a14b4fd8a6e0
szymek1 <bogusszymon2000@gmail.com>
2025-06-13 18:21:55 +0200

    Created initial testbench for alu+sign extension

---
+++ rv32i_sc.srcs/sim_1/new/alu_sign_extend_tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 06/13/2025 06:19:41 PM
+// Design Name: 
+// Module Name: alu_sign_extend_tb
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: Testbench integrating alu module with sign extension module.
+//              This testbench currently assumes that only load/store operations are
+//              supported.
+// 
+// Dependencies: rv32i_params.vh
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "rv32i_params.vh"
+
+
+module alu_sign_extend_tb(
+
+    );
+endmodule
