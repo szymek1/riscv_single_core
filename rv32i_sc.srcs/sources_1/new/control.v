fc0a38f90ba6dfa062811251cc223d27b256c6ac
szymek1 <bogusszymon2000@gmail.com>
2025-06-13 19:46:01 +0200

    Initial control module

---
+++ rv32i_sc.srcs/sources_1/new/control.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 06/13/2025 07:44:19 PM
+// Design Name: 
+// Module Name: control
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: Control module
+// 
+// Dependencies: rv32i_params.vh, rv32i_control.vh
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "rv32i_params.vh"
+`include "rv32i_control.vh"
+
+
+module control(
+    input  opcode,
+    output branch,
+    output mem_read,
+    output mem_2_reg,
+    output alu_op,
+    output mem_write,
+    output alu_src,
+    output reg_write
+    );
+endmodule
