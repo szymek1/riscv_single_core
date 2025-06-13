a4482ab2b2b2a36e69844bf69aa0799b17981f9a
szymek1 <bogusszymon2000@gmail.com>
2025-06-13 17:20:31 +0200

    Created initial alu module

---
+++ rv32i_sc.srcs/sources_1/new/alu.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 06/13/2025 05:19:21 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: ALU
+// 
+// Dependencies: rv32i_params.vh
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "rv32i_params.vh"
+
+
+module alu(
+    input alu_ctrl,
+    input alu_src,
+    input src1,
+    input src2,
+    output results,
+    output zero
+    );
+endmodule
