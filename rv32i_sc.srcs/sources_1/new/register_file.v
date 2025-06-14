b8af160cd40379459a1cc4a800f42d0311abe1a7
szymek1 <bogusszymon2000@gmail.com>
2025-06-14 19:44:01 +0200

    Initialized mani core module and did some esthetic changes to regfile

--- rv32i_sc.srcs/sources_1/new/register_file.v
+++ rv32i_sc.srcs/sources_1/new/register_file.v
@@ -35,9 +35,9 @@ module register_file(
     output wire  [`DATA_WIDTH-1:0]      rs2, // carries value that has been read with rs2_addr
     
     // WRITE
-    input  wire                      write_enable,
-    input  wire [`REG_ADDR_WIDTH-1:0]  write_addr, // specifies which register to write to
-    input  wire [`DATA_WIDTH-1:0]    write_data  // value to wrtie
+    input  wire                         write_enable,
+    input  wire [`REG_ADDR_WIDTH-1:0]   write_addr, // specifies which register to write to
+    input  wire [`DATA_WIDTH-1:0]       write_data  // value to wrtie
     
     );
     
---
+++ rv32i_sc.srcs/sources_1/new/riscv_cpu.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ISAE
+// Engineer: Szymon Bogus
+// 
+// Create Date: 06/14/2025 07:39:41 PM
+// Design Name: 
+// Module Name: riscv_cpu
+// Project Name: rv32i_sc
+// Target Devices: Zybo Z7-20
+// Tool Versions: 
+// Description: Main module assembling entire core
+// 
+// Dependencies: rv32i_params.vh, rv32i_control.vh
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "rv32i_params.vh"
+`include "rv32i_control.vh"
+
+
+module riscv_cpu(
+    input clk,
+    input rst
+    );
+endmodule
