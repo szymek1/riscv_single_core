7a2703991457f840ada911e82ed6348436b9a2a6
szymek1 <bogusszymon2000@gmail.com>
2025-06-19 11:51:20 +0200

    Added jal related option to sign extender module; reformatted previous options

--- rv32i_sc.srcs/sources_1/new/sign_extend.v
+++ rv32i_sc.srcs/sources_1/new/sign_extend.v
@@ -31,9 +31,10 @@ module sign_extend(
     
     always @(*) begin
         case (imm_src) 
-            3'b000 : imm_signed = {{20{src[24]}}, src[24:13]};                        // I-Type
-            3'b001 : imm_signed = {{20{src[24]}}, src[24:18], src[4:0]};              // S-Type
-            3'b010 : imm_signed = {{20{src[24]}}, src[0], src[23:18], src[4:1],1'b0}; // B-Type
+            3'b000 : imm_signed = {{20{src[24]}}, src[24:13]};                           // I-Type
+            3'b001 : imm_signed = {{20{src[24]}}, src[24:18], src[4:0]};                 // S-Type
+            3'b010 : imm_signed = {{20{src[24]}}, src[0], src[23:18], src[4:1], 1'b0};   // B-Type
+            3'b011 : imm_signed = {{12{src[24]}}, src[12:5], src[13], src[23:14], 1'b0}; // J-Type
             default: imm_signed = 32'd0;
         endcase
     end
